426d4774b899198cccd2fa39b2d65ecfd3b6b64b
ahmad hosseini <moze.mehraban@gmail.com>
2026-08-13 15:27:13 +0330

    deleted some useless files

--- src/app/orders/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function OrdersRedirectPage() {
-  redirect("/profile/orders");
-}
--- src/app/wishlist/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function WishlistRedirectPage() {
-  redirect("/profile/wishlist");
-}

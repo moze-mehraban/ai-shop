d520d975712f0e6544a5ceb26e24b2ec1942de94
ahmad hosseini <moze.mehraban@gmail.com>
2026-08-17 00:55:58 +0330

    Changes to be committed: 	modified:   .gitignore 	modified:   next.config.ts 	modified:   prisma/dev.db

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["0.0.0.0","192.168.100.13","255.255.255.255","151.234.194.130"],
+  allowedDevOrigins: ["0.0.0.0","192.168.100.13","255.255.255.255","151.234.194.130","127.0.0.1"],
   images: {
     remotePatterns: [
       {
